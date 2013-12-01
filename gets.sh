aeabe7147b3e3d50519015f67667743b982def7f
sonesuke <iamsonesuke@gmail.com>
2013-12-01 02:27:31 -0800

    modify

--- gets.sh
+++ gets.sh
@@ -10,9 +10,5 @@ git clone --recursive https://github.com/sonesuke/dotfiles.git
 cd dotfiles
 ./setup.sh
 
-cd .vim/bundle
-git clone https://github.com/Shougo/neobundle.vim.git
-cd ~
-
 cd ~/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure.git
 sudo ln -s "$PWD/pure.zsh" /usr/share/zsh/site-functions/pure_prompt_setup
